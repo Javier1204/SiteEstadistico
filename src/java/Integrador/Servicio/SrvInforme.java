8546964612a000fd2ed55c0993f3e8eb772bc160
=carlosTorresMedina <=carlosalfredo0108@gmail.com>
2016-12-01 03:58:46 -0500

    consulta informe terminado

---
+++ src/java/Integrador/Servicio/SrvInforme.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Integrador.Servicio;
+
+import Integrador.DAO.InformeDAO;
+import Integrador.DTO.InformeDTO;
+import java.util.List;
+
+/**
+ *
+ * @author carlos
+ */
+public class SrvInforme {
+    private static SrvInforme instance;
+    private InformeDAO daoInforme;
+    
+    protected SrvInforme(){
+      this.daoInforme=new InformeDAO();
+    }
+    
+    public static SrvInforme getInstance(){
+      if(instance==null){
+    instance=new SrvInforme();
+    }
+    return instance;
+    }
+    
+    /**
+    lista los informes
+    **/
+    public List<InformeDTO> listarPublicaciones(){
+    return this.daoInforme.listarPublicaciones();
+    }
+    
+    /**
+     * lista los informes segun el modulo
+     * @param modulo
+     * @return 
+     */
+    public List<InformeDTO> listarPublicacionesModulo(String modulo,int ano,int semestre){
+    return this.daoInforme.listarPublicacionesModulo(modulo,ano,semestre);
+    }
+    
+}
